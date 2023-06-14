d6a195a33c97841e00634cc43e35e384a14bbde9
Oleg <geoos1102@gmail.com>
2023-06-14 14:37:15 +0200

    прочитает его цену и название на главной странице

--- src/main/java/org/prog/pages/RozetkaPage.java
+++ src/main/java/org/prog/pages/RozetkaPage.java
@@ -1,13 +1,72 @@
 package org.prog.pages;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RozetkaPage extends AbstractPage {
 
-  private static final String URL = "https://rozetka.com.ua/";
+    private static final String URL = "https://rozetka.com.ua/";
 
-  public RozetkaPage(WebDriver driver) {
-    super(driver, URL);
-    this.driver = driver;
-  }
+    public RozetkaPage(WebDriver driver) {
+        super(driver, URL);
+        this.driver = driver;
+    }
+
+
+    public void setSearchValue(String value) {
+        driver.findElement(By.name("search")).sendKeys(value);
+    }
+
+    public void performSearch() {
+        driver.findElement(By.name("search")).sendKeys(Keys.ENTER);
+
+    }
+
+    public List<WebElement> getSearchResults() {
+        List<WebElement> searchLinks = new ArrayList<>();
+
+        List<WebElement> elements = new WebDriverWait(driver, Duration.ofSeconds(5L))
+                .until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".goods-tile")));
+
+        for (WebElement e : elements) {
+            try {
+                if (e.isDisplayed()) {
+                    searchLinks.add(e);
+                }
+            } catch (StaleElementReferenceException ignored) {
+                // Продолжаем выполнение цикла, если элемент стал устаревшим
+            }
+        }
+
+        return searchLinks;
+    }
+
+    public String getProductName() {
+        WebElement firstProduct = getSearchResults().get(0);
+        WebElement productNameElement = firstProduct.findElement(By.cssSelector(".goods-tile__title"));
+        return productNameElement.getText();
+    }
+
+    public String getProductPrice() {
+        WebElement firstProduct = getSearchResults().get(0);
+        WebElement productPriceElement = firstProduct.findElement(By.cssSelector(".goods-tile__price"));
+        return productPriceElement.getText();
+    }
+
+    public void clickAddToCartButton() {
+        WebElement firstProduct = getSearchResults().get(0);
+        WebElement addToCartButton = firstProduct.findElement(By.cssSelector(".buy-button"));
+        addToCartButton.click();
+    }
+
+
+
+
+
+
 }
--- src/test/java/org/prog/PageObjectTests.java
+++ src/test/java/org/prog/PageObjectTests.java
@@ -1,6 +1,5 @@
 package org.prog;
 
-import java.util.List;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.prog.pages.GooglePage;
@@ -12,31 +11,49 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PageObjectTests {
 
-  private PageFactory pageFactory;
+    private PageFactory pageFactory;
 
-  @BeforeSuite
-  public void setupDriver() {
-    pageFactory = new PageFactory(new ChromeDriver());
-  }
+    @BeforeSuite
+    public void setupDriver() {
+        pageFactory = new PageFactory(new ChromeDriver());
+    }
 
-  @Test
-  public void testGoogleUsingPageObjectWithChrome() {
-    GooglePage googlePage = (GooglePage) pageFactory.get(Pages.GOOGLE);
-    RozetkaPage rozetkaPage = (RozetkaPage) pageFactory.get(Pages.ROZETKA);
-    googlePage.loadPage();
-    googlePage.acceptCookiesIfPresent();
-    googlePage.setSearchValue("rozetka ua");
-    googlePage.performSearch();
-    List<WebElement> searchResults = googlePage.getSearchResults();
-    Assert.assertTrue(searchResults.size() > 1, "No search results found!");
-    searchResults.get(0).click();
-    Assert.assertTrue(rozetkaPage.isPageLoaded());
-  }
+    @Test
+    public void testGoogleUsingPageObjectWithChrome() {
+        GooglePage googlePage = (GooglePage) pageFactory.get(Pages.GOOGLE);
+        RozetkaPage rozetkaPage = (RozetkaPage) pageFactory.get(Pages.ROZETKA);
+        googlePage.loadPage();
+        googlePage.acceptCookiesIfPresent();
+        googlePage.setSearchValue("rozetka ua");
+        googlePage.performSearch();
+        List<WebElement> searchResults = googlePage.getSearchResults();
+        Assert.assertTrue(searchResults.size() > 1, "No search results found!");
+        searchResults.get(0).click();
+        Assert.assertTrue(rozetkaPage.isPageLoaded());
+        rozetkaPage.setSearchValue("Iphone");
+        rozetkaPage.performSearch();
 
-  @AfterSuite
-  public void tearDown() {
-    pageFactory.tearDown();
-  }
+        List<WebElement> searchResultsRozetka = rozetkaPage.getSearchResults();
+        Assert.assertFalse(searchResultsRozetka.isEmpty(), "No search results found on Rozetka page!");
+
+
+        Map<String, Object> productDetails = new HashMap<>();
+        productDetails.put("name", rozetkaPage.getProductName());
+        String productPrice = rozetkaPage.getProductPrice().replaceAll("\\D", ""); // Удаляем все нечисловые символы
+        productDetails.put("price", Integer.parseInt(productPrice));
+
+
+
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        pageFactory.tearDown();
+    }
 }
